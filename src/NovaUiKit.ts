3198291428e40d60c736e6fe5e43834fba7855e5
Egor Kozelskij <ekozelskiy@integrotechnologies.ru>
2026-05-08 19:55:19 +0300

    ref: nova-ui-kit NovaUiKit

---
+++ src/NovaUiKit.ts
@@ -0,0 +1,11 @@
+import { FLEX_SCHEMA_TYPE } from '@/components/Flex/types'
+import { GRID_SCHEMA_TYPE } from '@/components/Grid/types'
+import { TEXT_BLOCK_SCHEMA_TYPE } from '@/components/TextBlock/types'
+
+/** Группировка schema type для более читаемого UI Kit DSL. */
+export const NovaUiKit = {
+  Flex: FLEX_SCHEMA_TYPE,
+  Grid: GRID_SCHEMA_TYPE,
+  TextBlock: TEXT_BLOCK_SCHEMA_TYPE,
+} as const
+
